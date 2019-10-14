29a35d9731965a7ccafaf13e11304cb7a5dacd0f
MarkoC <marko.cedilnik@gmail.com>
2019-10-14 18:07:36 +0200

    dopolnil seznam

--- seznami.py
+++ seznami.py
@@ -3,5 +3,20 @@ stevila = [ 5, 2, 8, 3]
 #izpis vseh števil
 print(stevila)
 
+stevila.sort()
+print("Števila smo uredili")
+#izpis urejenih števil
+print(stevila)
+
 #izpis števila na mestu 1
-print(stevila[2])
+print("Število na mestu 1 je: ")
+print(stevila[1])
+
+#Izpis števila vseh elementov
+print("število vseh elementov je " + str(len(stevila)))
+
+stevila.append(7)
+stevila.remove(3)
+del stevila [0]
+print("spremenjen seznam: ")
+print(stevila)
